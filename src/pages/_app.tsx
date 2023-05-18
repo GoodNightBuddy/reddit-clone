0c743d62da9eff309090ce195b5efe21491b77da
GoodNightBuddy <fun.love.nuts@gmail.com>
2023-05-18 16:38:15 +0300

    Installed fonts

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import { theme } from '../chakra/theme';
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
