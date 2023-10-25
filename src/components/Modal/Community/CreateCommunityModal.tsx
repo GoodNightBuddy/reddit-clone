c4ad19edba8e4cd6d4bbdfc145df304a352a2b37
Pavlo Navrotskyi <fun.love.nuts@gmail.com>
2023-10-25 17:26:08 +0300

    Finished createComunitModal

--- src/components/Modal/Community/CreateCommunityModal.tsx
+++ src/components/Modal/Community/CreateCommunityModal.tsx
@@ -1,3 +1,4 @@
+import { CommunityDescription, CommunityTypes } from '@/src/types/enums';
 import {
   Button,
   Modal,
@@ -7,8 +8,18 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Box,
+  Divider,
+  Text,
+  Input,
+  Stack,
+  Checkbox,
+  Flex,
+  Icon,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { ReactEventHandler, useState } from 'react';
+import { BsFillEyeFill, BsFillPersonFill } from 'react-icons/bs';
+import { HiLockClosed } from 'react-icons/hi';
 
 type CreateCommunityModalProps = {
   open: boolean;
@@ -19,20 +30,128 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const [communityName, setCommunityName] = useState('');
+  const [charsRemaining, setCharsRemaining] = useState(21);
+  const [communityType, setCommunityType] = useState(CommunityTypes.Public);
+
+  const handleChange: ReactEventHandler<HTMLInputElement> = event => {
+    const { value } = event.currentTarget;
+    if (value.length > 21) return;
+
+    setCharsRemaining(21 - value.length);
+    setCommunityName(value);
+  };
+
+  const onCommunityNameChange: ReactEventHandler<HTMLInputElement> = event => {
+    setCommunityType(event.currentTarget.name as CommunityTypes);
+  };
+
   return (
     <>
-      <Modal isOpen={open} onClose={handleClose}>
+      <Modal isOpen={open} onClose={handleClose} size={'lg'}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>Modal Bodya</ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={handleClose}>
-              Close
+          <ModalHeader
+            display={'flex'}
+            flexDir={'column'}
+            fontSize={15}
+            padding={3}
+          >
+            Create a community
+          </ModalHeader>
+          <Box pl={3} pr={3}>
+            <Divider />
+            <ModalCloseButton />
+            <ModalBody display={'flex'} flexDir={'column'} padding={'10px 0px'}>
+              <Text fontWeight={600} fontSize={15}>
+                Name
+              </Text>
+              <Text fontSize={11} color={'gray.500'}>
+                Community names including capitalization cannot be change
+              </Text>
+              <Text
+                position={'relative'}
+                top={7}
+                left={'10px'}
+                w={5}
+                color={'gray.400'}
+              >
+                r/
+              </Text>
+              <Input
+                value={communityName}
+                size={'sm'}
+                pl='22px'
+                onChange={handleChange}
+                position={'relative'}
+              />
+              <Text
+                fontSize={'9pt'}
+                color={charsRemaining === 0 ? 'red' : 'gray.500'}
+              >
+                {charsRemaining}Characters remaining
+              </Text>
+              <Box mt={4} mb={4}>
+                <Text fontWeight={600} fontSize={15}>
+                  Community type
+                </Text>
+                <Stack spacing={2}>
+                  <Checkbox
+                    name={CommunityTypes.Public}
+                    isChecked={communityType === CommunityTypes.Public}
+                    onChange={onCommunityNameChange}
+                  >
+                    <Flex align={'center'}>
+                      <Icon as={BsFillPersonFill} color={'gray.500'} mr={2} />
+                      <Text fontSize={'10pt'} mr={1}>
+                        {CommunityTypes.Public}
+                      </Text>
+                      <Text fontSize={'8pt'} color={'gray.500'} pt={1}>
+                        {CommunityDescription.Public}
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name={CommunityTypes.Restricted}
+                    isChecked={communityType === CommunityTypes.Restricted}
+                    onChange={onCommunityNameChange}
+                  >
+                    <Flex align={'center'}>
+                      <Icon as={BsFillEyeFill} color={'gray.500'} mr={2} />
+                      <Text fontSize={'10pt'} mr={1}>
+                        {CommunityTypes.Restricted}
+                      </Text>
+                      <Text fontSize={'8pt'} color={'gray.500'} pt={1}>
+                        {CommunityDescription.Restricted}
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name={CommunityTypes.Private}
+                    isChecked={communityType === CommunityTypes.Private}
+                    onChange={onCommunityNameChange}
+                  >
+                    <Flex align={'center'}>
+                      <Icon as={HiLockClosed} color={'gray.500'} mr={2} />
+                      <Text fontSize={'10pt'} mr={1}>
+                        {CommunityTypes.Private}
+                      </Text>
+                      <Text fontSize={'8pt'} color={'gray.500'} pt={1}>
+                        {CommunityDescription.Private}
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                </Stack>
+              </Box>
+            </ModalBody>
+          </Box>
+          <ModalFooter bg={'gray.100'} borderRadius={'0px 0px 10px 10px'}>
+            <Button variant={'outline'} h={'30px'} mr={3} onClick={handleClose}>
+              Cancel
             </Button>
-            <Button variant='ghost'>Secondary Action</Button>
+            <Button h={'30px'} onClick={() => {}}>
+              Create Community
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
--- src/types/enums.ts
+++ src/types/enums.ts
@@ -9,3 +9,15 @@ export enum AuthModalTitles {
   Signup = 'Sign up',
   ResetPassword = 'Reset password',
 }
+
+export enum CommunityTypes {
+  Public = 'Public',
+  Restricted = 'Restricted',
+  Private = 'Private',
+}
+
+export enum CommunityDescription {
+  Public = 'Anyone can view, post, and comment this community',
+  Restricted = 'Anyone can view, but only approved users can post',
+  Private = 'Only approved users can view and submit to this community',
+}
